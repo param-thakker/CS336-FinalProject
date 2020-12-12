a45f23d6bd2e786ac84cb8e8659e5f672fce64ca
kreddy797 <75705049+kreddy797@users.noreply.github.com>
2020-12-12 17:33:29 -0500

    Add files via upload

--- ProjectPart3.sql
+++ ProjectPart3.sql
@@ -18,8 +18,8 @@ CREATE TABLE IF NOT EXISTS EmployeeWorksAt(
 );
 CREATE TABLE IF NOT EXISTS TransitLine(
 				Transit_Line_Name varchar(20) primary key,
-                Origin varchar(20),
-                Destination varchar(20),
+                Origin varchar(50),
+                Destination varchar(50),
                 ArrivalTime datetime,
                 DepartureTime datetime,
                 Train_Number int,
@@ -30,8 +30,8 @@ CREATE TABLE IF NOT EXISTS hasStop(
 				Transit_Line_Name varchar(20),
                 StationID int,
                 Fare float,
-                Arrival datetime,
                 Departure datetime,
+                Arrival datetime,
                 primary key (Transit_Line_Name, StationID),
 				foreign key (Transit_Line_Name) references TransitLine(Transit_Line_Name),
                 foreign key (StationID) references Station(StationID)
@@ -41,38 +41,28 @@ CREATE TABLE IF NOT EXISTS TrainPartOf(
 				Transit_Line_Name varchar(20),
                 foreign key (Transit_Line_Name) references TransitLine(Transit_Line_Name)
 );
-CREATE TABLE IF NOT EXISTS Customer(
-				Email varchar(50),
-                Password varchar(50),
-                First_Name varchar(20),
-                Last_Name varchar(20),
-                Username varchar(20) primary key
-);
-CREATE TABLE IF NOT EXISTS ResPassTransLine(
-				Reservation_Number varchar(20),
-                Reservation_Date date,
-                Fare float,
-				Username varchar(20),
-                Transit_Line_Name varchar(20),
-                Origin varchar(20),
-                Destination varchar(20),
-                DepartureTime datetime,
-                primary key(Reservation_Number),
-                foreign key (Transit_Line_Name) references TransitLine(Transit_Line_Name),
-                foreign key (Username) references Customer(Username)
-);
 CREATE TABLE IF NOT EXISTS Accounts(
-				First_Name varchar(50),
-                Last_Name varchar(50),
 				Email varchar(50),
 				Password varchar(50),
                 Username varchar(50),
                 Usertype varchar(20),
-                primary key(Username));
-                
+                primary key(Username)         
+);
+CREATE TABLE IF NOT EXISTS ResPassTransLine(
+				Reservation_Number int,
+                Reservation_Date date,
+				Username varchar(20),
+                Transit_Line_Name varchar(20),
+                Origin varchar(50),
+                Destination varchar(50),
+                DepartureTime datetime,
+                Fare float,
+                primary key(Reservation_Number),
+                foreign key (Transit_Line_Name) references TransitLine(Transit_Line_Name),
+                foreign key (Username) references Accounts(Username)
+);
 CREATE TABLE IF NOT EXISTS Questions(
 				Question varchar(50),
                 Answer varchar(50),
-                primary key(Question));         
+                primary key(Question)
 );
-
