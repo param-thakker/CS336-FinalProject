9a9f3292aa2905f68f8dc31e86ca165bdcd74316
kreddy797 <75705049+kreddy797@users.noreply.github.com>
2020-12-13 20:42:00 -0500

    added update cascade

--- ProjectPart3.sql
+++ ProjectPart3.sql
@@ -1,3 +1,4 @@
+
 CREATE DATABASE IF NOT EXISTS TRAIN;
 USE TRAIN;
 CREATE TABLE IF NOT EXISTS Station(
@@ -14,7 +15,7 @@ CREATE TABLE IF NOT EXISTS EmployeeWorksAt(
                 Username varchar(20),
                 Password varchar(20),
                 primary key(SSN),
-                foreign key(StationID) references Station(StationID)
+                foreign key(StationID) references Station(StationID) on update cascade
 );
 CREATE TABLE IF NOT EXISTS TransitLine(
 				Transit_Line_Name varchar(20) primary key,
@@ -33,13 +34,13 @@ CREATE TABLE IF NOT EXISTS hasStop(
                 Departure datetime,
                 Arrival datetime,
                 primary key (Transit_Line_Name, StationID),
-				foreign key (Transit_Line_Name) references TransitLine(Transit_Line_Name),
-                foreign key (StationID) references Station(StationID)
+				foreign key (Transit_Line_Name) references TransitLine(Transit_Line_Name) on update cascade,
+                foreign key (StationID) references Station(StationID) on update cascade
 );
 CREATE TABLE IF NOT EXISTS TrainPartOf(
 				TrainID int primary key,
 				Transit_Line_Name varchar(20),
-                foreign key (Transit_Line_Name) references TransitLine(Transit_Line_Name)
+                foreign key (Transit_Line_Name) references TransitLine(Transit_Line_Name) on update cascade
 );
 CREATE TABLE IF NOT EXISTS Customer(
 				Email varchar(50),
@@ -47,6 +48,15 @@ CREATE TABLE IF NOT EXISTS Customer(
                 First_Name varchar(20),
                 Last_Name varchar(20),
                 Username varchar(20) primary key
+);
+CREATE TABLE IF NOT EXISTS Accounts(
+				First_Name varchar(50),
+                Last_Name varchar(50),
+				Email varchar(50),
+				Password varchar(50),
+                Username varchar(50),
+                Usertype varchar(20),
+                primary key(Username)
 );
 CREATE TABLE IF NOT EXISTS ResPassTransLine(
 				Reservation_Number varchar(20),
@@ -58,17 +68,9 @@ CREATE TABLE IF NOT EXISTS ResPassTransLine(
                 Destination varchar(20),
                 DepartureTime datetime,
                 primary key(Reservation_Number),	
-                foreign key (Transit_Line_Name) references TransitLine(Transit_Line_Name),
-                foreign key (Username) references Customer(Username)
+                foreign key (Transit_Line_Name) references TransitLine(Transit_Line_Name) on update cascade,
+                foreign key (Username) references Customer(Username) on update cascade
 );
-CREATE TABLE IF NOT EXISTS Accounts(
-				First_Name varchar(50),
-                Last_Name varchar(50),
-				Email varchar(50),
-				Password varchar(50),
-                Username varchar(50),
-                Usertype varchar(20),
-                primary key(Username));
                 
 CREATE TABLE IF NOT EXISTS Questions(
 				Question varchar(50),
